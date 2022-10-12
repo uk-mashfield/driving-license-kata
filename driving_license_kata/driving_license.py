b07cee13dda31bc62e2574923e334147ebd59b37
Mark Ashfield <mark.ashfield@codurance.com>
2022-10-12 13:07:24 +0100

    Added fixture to unit tests

--- driving_license_kata/driving_license.py
+++ driving_license_kata/driving_license.py
@@ -1,10 +1,13 @@
 class DrivingLicense:
-  
-  def __init__(self,data:list) -> None:
-    self._data = data
-    
-  def get_license_number(self) -> str:
-    pass
-  
-  def get_surname(self) -> str:
-    return "SMITH"
+
+    def __init__(self, data: list) -> None:
+        self._data = data
+
+    def get_license_number(self) -> str:
+        pass
+
+    def get_surname(self) -> str:
+        return self._data[2].upper()[:5].ljust(5, '9')
+
+    def get_decade(self) -> str:
+        return self._data[3][-2]
--- tests/unit/test_driving_license_number.py
+++ tests/unit/test_driving_license_number.py
@@ -1,10 +1,29 @@
 from driving_license_kata.driving_license import DrivingLicense
 import pytest
 
-def test_should_return_first_5_characters_of_surname():
-  data = ["John","James","Smith","01-Jan-2000","M"]
-  surname = DrivingLicense(data).get_surname()
-  assert surname == "SMITH"
-  
-def test_should_return_surname_with_less_than_5_chars():
-  pass
+
+@pytest.fixture
+def driving_license_generator():
+    return DrivingLicense
+
+
+@pytest.mark.parametrize("data, expected",
+                         [
+                             (["John", "James", "Smith", "01-Jan-2000", "M"], "SMITH"),
+                             (["John", "James", "Smit", "01-Jan-2000", "M"], "SMIT9"),
+                             (["John", "James", "SmithtyHughes", "01-Jan-2000", "M"], "SMITH")
+                         ])
+def test_should_ensure_surname_is_formatted_correctly(data, expected, driving_license_generator: DrivingLicense):
+    surname = driving_license_generator(data).get_surname()
+    assert surname == expected
+
+
+@pytest.mark.parametrize("data, expected",
+                         [
+                             (["John", "James", "Smith", "01-Jan-2050", "M"], "5"),
+                             (["John", "James", "Smith", "01-Jan-2090", "M"], "9"),
+                             (["John", "James", "Smith", "01-Jan-2000", "M"], "0")
+                         ])
+def test_extract_the_decade_from_the_year_of_birth(data, expected, driving_license_generator: DrivingLicense):
+    decade = driving_license_generator(data).get_decade()
+    assert decade == expected
